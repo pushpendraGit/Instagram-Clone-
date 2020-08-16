25fcbce06ebaed92e686121a4d1e958bdd5b44b3
Pushpendra Singh <pushpendra7060@gmail.com>
2020-08-16 13:27:40 +0530

    header Design is completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import './App.css';
+import Post from './Post';
 
 function App() {
   return (
-    <div className="App">
-      <h1>App
-      </h1>
+    <div className="app">
+    <div className="app__header"> 
+    <img className="app__headerImage" 
+    src="https://pngimage.net/wp-content/uploads/2018/06/font-instagram-png-2.png" />
+
+    </div>
+
+    <Post />
     
     </div>
   );
